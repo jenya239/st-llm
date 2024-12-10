e25caa27fecdade93b6547ab7be3bcb3bb382815
Arshansky Evgeny <arshansky@nordpointlp.com>
2024-12-10 13:30:29 +0400

    brainstorm wip

--- st-llm.py
+++ st-llm.py
@@ -12,6 +12,9 @@ default_system_role = "You are a helpful assistant. Provide very short answers w
 default_color = "cyanish"
 default_color_code = "#2AA198"
 
+brainstorm_role = "You are a participant in a brainstorming session with other models. Your task is to give short answers and quickly reach a consensus. The answers of other participants will be transmitted in one message in the format 'name: message'. a simpler, less enthusiastic tone and mention the names of the interlocutors, sometimes ask them who they are, keep the smalltalk going. you can joke"
+brainstorm_rounds = 10
+
 class Provider:
 	def __init__(self, name, settings, key):
 		self.name = name
@@ -24,7 +27,7 @@ class Provider:
 		self.port = settings.get("port", default_port)
 		self.conn = http.client.HTTPSConnection(self.host, self.port) if self.protocol == "https" else http.client.HTTPConnection(self.host, self.port)
 		self.system_role = None
-		self.set_system_role(settings.get("system_role", default_system_role))
+		self.restore_role()
 		self.color = settings.get("color", default_color)
 		self.color_code = settings.get("color_code", default_color_code)
 
@@ -32,6 +35,9 @@ class Provider:
 		if self.system_role != system_role:
 			self.system_role = system_role
 			self.init_messages()
+
+	def restore_role(self):
+		self.set_system_role(self.settings.get("system_role", default_system_role))
 
 	def send_request(self):
 		headers = {
@@ -85,10 +91,11 @@ class OpenAIChatAPI(Provider):
 		self.messages.append({ "role": "assistant", "content": message })
 
 	def get_answer(self, response_json):
+		print(response_json)
 		return response_json["choices"][0]["message"]["content"]
 
 
-class AnthropicAPI(Provider):
+class AnthropicAPI(Provider): 
 	def init_messages(self):
 		self.messages = []
 
@@ -197,28 +204,49 @@ class XAIChatAPI(Provider):
 		return response_json["choices"][0]["message"]["content"]
 
 
+class Brainstorm:
+	def __init__(self, providers):
+		self.providers = providers
+
+	def chat(self, message):
+		print('brainstorm')
+		return "brainstorm {}".format(message)
+
+	def start(self):
+		for provider in self.providers:
+			provider.set_system_role(brainstorm_role)
+
+
+
 providers = {}
 provider = None
+brainstorm = None
 
 def plugin_loaded():
-	global provider, providers
+	global provider, providers, brainstorm
 	settings = sublime.load_settings("st-llm.sublime-settings")
 	active_provider = settings.get("active_provider", default_provider)
 	keys = settings.get("keys", {})
 
 	provider_settings = settings.get(active_provider, {})
 
-	providers['openai'] = OpenAIChatAPI("openai", provider_settings, keys.get("openai", ""))
-	providers['anthropic'] = AnthropicAPI("anthropic", provider_settings, keys.get("anthropic", ""))
-	providers['gemini'] = GeminiAPI("gemini", provider_settings, keys.get("gemini", ""))
-	providers['ollama'] = OllamaAPI("ollama", provider_settings, "")
-	providers['xai'] = XAIChatAPI("xai", provider_settings, keys.get("xai", ""))
+	providers['openai'] = OpenAIChatAPI("openai", settings.get("openai", {}), keys.get("openai", ""))
+	providers['anthropic'] = AnthropicAPI("anthropic", settings.get("anthropic", {}), keys.get("anthropic", ""))
+	providers['gemini'] = GeminiAPI("gemini", settings.get("gemini", {}), keys.get("gemini", ""))
+	providers['ollama'] = OllamaAPI("ollama", settings.get("ollama", {}), "")
+	providers['xai'] = XAIChatAPI("xai", settings.get("xai", {}), keys.get("xai", ""))
 
 	provider = providers[active_provider]
+
+	brainstorm_providers = [providers['openai'], providers['anthropic'], providers['gemini']]
+	brainstorm = Brainstorm(brainstorm_providers)
 	print("Plugin loaded successfully! active_provider = "+provider.name)
 
 class StLlmCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
+		for p in providers.values():
+			p.restore_role()
+
 		cursor_position = self.view.sel()[0].begin()
 		line_region = self.view.line(cursor_position)
 
@@ -229,6 +257,37 @@ class StLlmCommand(sublime_plugin.TextCommand):
 			self.view.add_regions(key, [line_region], "region." + provider.color, "circle", 
 				sublime.DRAW_NO_OUTLINE, [provider.model_name], provider.color_code) # | sublime.DRAW_SQUIGGLY_UNDERLINE
 			self.view.insert(edit, cursor_position, "\n\n{}\n\n".format(answer))
+			self.view.show_at_center(line_region)
+		else:
+			print("The current line is empty.")
+
+class StLlmBrainstormCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		brainstorm.start()
+
+		cursor_position = self.view.sel()[0].begin()
+		line_region = self.view.line(cursor_position)
+
+		current_line_text = self.view.substr(line_region)
+		if current_line_text.strip():
+			answer = brainstorm.chat(current_line_text)
+			key = "key_" + str(random.randint(1000, 9999))
+			self.view.add_regions(key, [line_region], "region.cyanish", "circle", 
+				sublime.DRAW_NO_OUTLINE, ['brainstorm'], '#2AA198') # | sublime.DRAW_SQUIGGLY_UNDERLINE
+
+			messages = [{"role": "user", "content": current_line_text}]
+			for r in range(brainstorm_rounds):
+				for i, provider in enumerate(brainstorm.providers):
+					msg = ""
+					for m in messages:
+						msg += "{}: {}\n\n".format(m["role"], m["content"])
+					print(msg)
+					answer = provider.chat(msg)
+					messages.append({"role": provider.name, "content": answer})
+					cursor_position = self.view.sel()[0].begin()
+					self.view.insert(edit, cursor_position, "\n\n{}: {}".format(provider.name, answer))
+				# messages = []
+
 			self.view.show_at_center(line_region)
 		else:
 			print("The current line is empty.")
